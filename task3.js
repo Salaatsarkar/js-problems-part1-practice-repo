87f03f64f7911a70ab325120a6aacaf1b83bdd6b
Salaatsarkar <salaatsarkar0501@gmail.com>
2026-02-09 23:37:37 +0400

    count vowels

--- task3.js
+++ task3.js
@@ -1,3 +1,17 @@
 // ### Task-3: 
 // Write a function to count the number of vowels in a string.
 
+function countVowels(str)
+{
+    let count = 0;
+    for(let word of str)
+    {
+        if(word === "a"|| word ==="e" || word ==="i" || word ==="o"|| word ==="u")
+        {
+            count ++;
+        }
+    }
+    return count;
+}
+const totalVowel = countVowels("azaan");
+console.log(totalVowel);
